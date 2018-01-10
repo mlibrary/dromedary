4ad3785e16dc61a599c1d1c064b4924fce2111d5
Bill Dueber <bill@dueber.com>
2018-01-10 17:30:30 -0500

    Start messing wiht javascript

--- app/assets/javascripts/blacklight/autocomplete.js
+++ app/assets/javascripts/blacklight/autocomplete.js
@@ -1,37 +1,43 @@
 /*global Bloodhound */
 
-Blacklight.onLoad(function() {
-  'use strict';
 
-  $('[data-autocomplete-enabled="true"]').each(function() {
-    var $el = $(this);
-    if($el.hasClass('tt-hint')) {
-      return;
-    }
-    var suggestUrl = $el.data().autocompletePath;
-    
-    var terms = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      remote: {
-        url: suggestUrl + '?q=%QUERY',
-        wildcard: '%QUERY',
-      }
-    });
-    
-    terms.initialize();
+Blacklight.onLoad(function () {
+    'use strict';
 
-    $el.typeahead({
-      hint: true,
-      highlight: true,
-      minLength: 2
-    },
-    {
-      name: 'terms',
-      displayKey: 'term',
-      source: terms.ttAdapter(),
-        limit: 15
+    $('[data-autocomplete-enabled="true"]').each(function () {
+        var $el = $(this);
+        if ($el.hasClass('tt-hint')) {
+            return;
+        }
+        var suggestUrl = $el.data().autocompletePath;
+
+        var terms = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                url: suggestUrl + '?q=%QUERY',
+                wildcard: '%QUERY',
+            }
+        });
+
+        terms.initialize();
+
+        $el.typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 2,
+                callback: {
+                    onClickAfter: function (node, a, item, event) {
+                        alert("Click");
+                    }
+                },
+            },
+            {
+                name: 'terms',
+                displayKey: 'term',
+                source: terms.ttAdapter(),
+                limit: 15
+            });
     });
-  });
 });
 
