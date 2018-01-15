4dfacb5f4ee93a3d8c1b7eb53f36ae76497a4750
Bill Dueber <bill@dueber.com>
2018-01-15 13:56:30 -0500

    Derive solr core name as well

--- indexer/reload_solr_config.rb
+++ indexer/reload_solr_config.rb
@@ -8,9 +8,18 @@ config_dir = Pathname(__dir__).realdirpath.parent + 'config'
 blacklight_yaml = config_dir + 'blacklight.yml'
 blacklight_config = YAML.load(ERB.new(File.read(blacklight_yaml)).result)
 
+uri  = URI(ENV['SOLR_URL'] || blacklight_config['production']['url'])
 
-solr_url  = ENV['SOLR_URL'] || blacklight_config['production']['url']
+puts 'URI is #{uri.to_s}'
+path = uri.path.split('/')
+corename = path.pop
+uri.path = path.join('/') # go up a level -- we popped off the core name
+solr_url = uri.to_s
+
+puts "SOLR URL is #{solr_url}"
 
 client = SimpleSolrClient::Client.new(solr_url)
-med    = client.core('med')
-med.reload
+
+require 'pry'; binding.pry
+core    = client.core(corename)
+core.reload
