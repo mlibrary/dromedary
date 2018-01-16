d659698300c780e09c93f4388aa98f5d74a686b9
Bill Dueber <bill@dueber.com>
2018-01-16 12:18:04 -0500

    Tear out all the bootsnap stuff just to try to get production running

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,27 +72,26 @@ Rails.application.configure do
   # be put inside of Dir.tmpdir / railscache
   # Size is on the order of 50MB
 
-  env = ENV['RAILS_ENV'] || 'development'
-
-  parent_dir_for_cached_files = "/tmp/" # Dir.tmpdir
-  bootstrap_tempdir           = 'dromedary'
-  cachedir                    = Pathname.new(parent_dir_for_cached_files).realdirpath + 'railscache' + bootstrap_tempdir
-  use_bootsnap = %w[development test].include?(env) and ENV["CI"] != true
-
-
-  if use_bootsnap
-    require 'bootsnap'
-    cachedir.mkpath
-    Bootsnap.setup(
-        cache_dir: cachedir.to_s, # Path to your cache
-        development_mode: true, # Current working environment, e.g. RACK_ENV, RAILS_ENV, etc
-        load_path_cache: true, # Optimize the LOAD_PATH with a cache
-        autoload_paths_cache: true, # Optimize ActiveSupport autoloads with cache
-        disable_trace: true, # (Alpha) Set `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
-        compile_cache_iseq: true, # Compile Ruby code into ISeq cache, breaks coverage reporting.
-        compile_cache_yaml: true # Compile YAML into a cache
-    )
-
-  end
+  # env = ENV['RAILS_ENV'] || 'development'
+  #
+  # parent_dir_for_cached_files = "/tmp/" # Dir.tmpdir
+  # bootstrap_tempdir           = 'dromedary'
+  # cachedir                    = Pathname.new(parent_dir_for_cached_files).realdirpath + 'railscache' + bootstrap_tempdir
+  # use_bootsnap = %w[development test].include?(env) and ENV["CI"] != true
+  #
+  #
+  # if use_bootsnap
+  #   require 'bootsnap'
+  #   cachedir.mkpath
+  #   Bootsnap.setup(
+  #       cache_dir: cachedir.to_s, # Path to your cache
+  #       development_mode: true, # Current working environment, e.g. RACK_ENV, RAILS_ENV, etc
+  #       load_path_cache: true, # Optimize the LOAD_PATH with a cache
+  #       autoload_paths_cache: true, # Optimize ActiveSupport autoloads with cache
+  #       disable_trace: true, # (Alpha) Set `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
+  #       compile_cache_iseq: true, # Compile Ruby code into ISeq cache, breaks coverage reporting.
+  #       compile_cache_yaml: true # Compile YAML into a cache
+  #   )
+  # end
 
 end
