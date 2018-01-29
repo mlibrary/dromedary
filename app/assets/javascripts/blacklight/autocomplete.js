0f296bc823b1f1e1cfc8d66119680653f8487c8e
Bill Dueber <bill@dueber.com>
2018-01-29 11:09:54 -0500

    Add data-attribute "autocomplete_solr_handler" to single-box search form to allow changing autocomplete target.
    Changing this data value will change which solr handler is targeted
    for getting autocomplete options. It needs to be changed by any
    change in the dropdown.

--- app/assets/javascripts/blacklight/autocomplete.js
+++ app/assets/javascripts/blacklight/autocomplete.js
@@ -1,41 +1,44 @@
 /*global Bloodhound */
 
+var blacklight_autocomplete = function () {
+    var $el = $(this);
+    if ($el.hasClass('tt-hint')) {
+        return;
+    }
+    var suggestUrl = $el.data().autocompletePath;
+    var autocomplete_solr_handler = $el.data().autocompleteSolrHandler;
+
+    var terms = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: suggestUrl + '?autocomplete_solr_handler=' + autocomplete_solr_handler + '&q=%QUERY',
+            wildcard: '%QUERY',
+        }
+    });
+
+    terms.initialize();
+
+    $el.typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 2
+        },
+        {
+            name: 'terms',
+            displayKey: 'term',
+            source: terms.ttAdapter(),
+            limit: 15
+        }).on('typeahead:selected',
+        function(event, data) {
+            $(event.target).closest('form').submit();
+        });
+};
+
 
 Blacklight.onLoad(function () {
     'use strict';
 
-    $('[data-autocomplete-enabled="true"]').each(function () {
-        var $el = $(this);
-        if ($el.hasClass('tt-hint')) {
-            return;
-        }
-        var suggestUrl = $el.data().autocompletePath;
-
-        var terms = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            remote: {
-                url: suggestUrl + '?q=%QUERY',
-                wildcard: '%QUERY',
-            }
-        });
-
-        terms.initialize();
-
-        $el.typeahead({
-                hint: true,
-                highlight: true,
-                minLength: 2
-            },
-            {
-                name: 'terms',
-                displayKey: 'term',
-                source: terms.ttAdapter(),
-                limit: 15
-            }).on('typeahead:selected',
-            function(event, data) {
-                $(event.target).closest('form').submit();
-            });
-    });
+    $('[data-autocomplete-enabled="true"]').each(blacklight_autocomplete);
 });
 
