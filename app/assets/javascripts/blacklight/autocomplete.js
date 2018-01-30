cafd6c40adffd87009c6bbdf49f729263419d1c1
Bill Dueber <bill@dueber.com>
2018-01-30 13:46:28 -0500

    Override some blacklight stuff to allow multiple autocomplete fields
    See the readme. Man, what a mess.

--- app/assets/javascripts/blacklight/autocomplete.js
+++ app/assets/javascripts/blacklight/autocomplete.js
@@ -5,19 +5,21 @@ var blacklight_autocomplete = function () {
     if ($el.hasClass('tt-hint')) {
         return;
     }
-    var suggestUrl = $el.data().autocompletePath;
-    var autocomplete_solr_handler = $el.data().autocompleteSolrHandler;
+    var suggestUrl = $el.data('autocomplete-path');
+    var autocomplete_config = $el.data('autocomplete-config');
 
     var terms = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         remote: {
-            url: suggestUrl + '?autocomplete_solr_handler=' + autocomplete_solr_handler + '&q=%QUERY',
-            wildcard: '%QUERY',
+            url: suggestUrl + '?autocomplete_config=' + autocomplete_config + '&q=%QUERY',
+            wildcard: '%QUERY'
         }
     });
 
-    terms.initialize();
+    terms.initialize(true); // true means "reinitialixe every time" ???
+
+    $el.typeahead('destroy');
 
     $el.typeahead({
             hint: true,
@@ -45,16 +47,32 @@ var blacklight_set_autocomplete = function(i, rawel) {
             function() {
                 var me = $(this);
                 el.data('autocomplete-config', me.val());
-                alert("Controller value is " + el.data('autocomplete-config'));
-
+                $.each([el], blacklight_autocomplete);
             });
+    }
+    el.trigger('change')
+};
+
+var setup_controller = function(i, rawel) {
+    var el = $(rawel);
+    var controller_id = el.data('autocomplete-controller');
+    if (controller_id) {
+        var controller = $('#' + controller_id);
+        controller.change();
     }
 };
 
 
 Blacklight.onLoad(function () {
     'use strict';
-    $('[data-autocomplete-enabled="true"]').each(blacklight_autocomplete);
+    // Set up the connection between the controllers and the inputs
     $('[data-autocomplete-enabled="true"]').each(blacklight_set_autocomplete);
+
+    // Add the autocomplete
+    $('[data-autocomplete-enabled="true"]').each(blacklight_autocomplete);
+
+    // Inititalize with the current value of the controller.
+    $('[data-autocomplete-enabled="true"]').each(setup_controller);
+
 });
 
---
+++ lib/dromedary/forms/search_form.rb
@@ -0,0 +1,13 @@
+
+module Dromedary
+  module Forms
+    class SearchForm
+
+      incude ActionView::Helpers::FormOptionsHelper
+      include Blacklight::CatalogHelperBehavior
+
+
+    end
+  end
+end
+
