c9048fa33d6f40bec12f5f52683b6da7febe20e6
Bill Dueber <bill@dueber.com>
2018-02-21 14:49:48 -0500

    Index more of the Things in an entry

--- indexer/main_indexer.rb
+++ indexer/main_indexer.rb
@@ -27,7 +27,7 @@ end
 # end
 
 def entry_field(name)
-  ->(rec, acc) { acc << rec.send(name)}
+  ->(rec, acc) {acc << rec.send(name)}
 end
 
 
@@ -46,28 +46,53 @@ to_field 'sequence', entry_field(:sequence)
 
 
 # headwords and forms
-to_field 'official_headword' , entry_field(:original_headwords)
+to_field 'official_headword', entry_field(:original_headwords)
 to_field 'headword', entry_field(:regularized_headwords)
 to_field 'orth', entry_field(:all_forms)
 
-# Definitions
+# Definitions and modern equivalents
 to_field('definition_text') do |entry, acc|
   acc.replace entry.senses.flat_map(&:definition_text)
+end
+
+to_field('oed_norm') do |entry, acc|
+  acc << entry.oedlink.norm
 end
 
 # Usages
 #      * create tmaps
 to_field('discipline_usage') do |entry, acc|
-  acc.replace entry.senses.flat_map(&:discipline_usages)
+  acc.replace entry.senses.flat_map(&:discipline_usages).compact.uniq
 end
 
 to_field('grammatical_usage') do |entry, acc|
-  acc.replace entry.senses.flat_map(&:grammatical_usages)
+  acc.replace entry.senses.flat_map(&:grammatical_usages).compact.uniq
 end
 
+# Etymology and pos
 to_field 'etyma_language', entry_field(:etym_languages)
-to_field 'pos_raw', entry_field(:normalized_pos_raw)
+to_field 'pos_raw', entry_field(:pos_raw)
+to_field 'pos_abbrev', entry_field(:normalized_pos_raw)
 
+# Quotes
+to_field('quote_text') do |entry, acc|
+  acc.replace entry.all_quotes.map(&:text)
+end
+to_field('quote_md') do |entry, acc|
+  acc.replace entry.all_citations.flat_map(&:md).uniq.compact
+end
+to_field('quote_cd') do |entry, acc|
+  acc.replace entry.all_citations.flat_map(&:cd).uniq.compact
+end
+to_field('quote_title') do |entry, accc|
+  acc.replace entry.all_citations.flat_map(&:bibs).flat_map(&:stencil).flat_map(&:title)
+end
+to_field('quote_manuscript') do |entry, accc|
+  acc.replace entry.all_citations.flat_map(&:bibs).flat_map(&:stencil).flat_map(&:ms)
+end
+to_field('quote_rid') do |entry, accc|
+  acc.replace entry.all_citations.flat_map(&:bibs).flat_map(&:stencil).flat_map(&:rid)
+end
 
 
 
