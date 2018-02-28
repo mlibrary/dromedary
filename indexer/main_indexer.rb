85ff54ff05bbbf8568d9ff8b614ec5bea85bd8e5
Bill Dueber <bill@dueber.com>
2018-02-28 18:53:14 -0500

    Update schema and indexing code

--- indexer/main_indexer.rb
+++ indexer/main_indexer.rb
@@ -1,6 +1,9 @@
 require 'middle_english_dictionary'
-require_relative '../lib/annoying_utilities'
-require_relative '../lib/med_installer'
+
+$LOAD_PATH.unshift Pathname.new(__dir__).parent + 'lib'
+require 'annoying_utilities'
+
+require 'med_installer'
 
 settings do
   provide "log.batch_progress", 5_000
@@ -27,7 +30,7 @@ end
 # end
 
 def entry_method(name)
-  ->(rec, acc) {acc << rec.send(name)}
+  ->(rec, acc) {acc.replace Array(rec.send(name))}
 end
 
 
@@ -53,6 +56,22 @@ to_field 'official_headword', entry_method(:original_headwords)
 to_field 'headword', entry_method(:regularized_headwords)
 to_field 'orth', entry_method(:all_forms)
 
+# We need to do the word sugggestions here (instead of in schema.xml
+# with copyField) because copyField allows duplicates.
+
+to_field "word_suggestions", entry_method(:all_forms)
+to_field('headword_only_suggestions') do |entry, acc|
+  hw = [entry.regularized_headwords, entry.original_headwords].flatten.uniq
+  acc.replace hw
+end
+
+# Etymology and pos
+to_field 'etyma_language', entry_method(:etym_languages)
+
+
+to_field 'pos_raw', entry_method(:pos_raw)
+to_field 'pos_abbrev', entry_method(:normalized_pos_raw)
+
 # Definitions and modern equivalents
 to_field('definition_text') do |entry, acc|
   acc.replace entry.senses.flat_map(&:definition_text)
@@ -61,6 +80,37 @@ end
 to_field('oed_norm') do |entry, acc|
   acc << entry.oedlink.norm if entry.oedlink
 end
+
+# Quotes
+to_field('quote_text') do |entry, acc|
+  acc.replace entry.all_quotes.map(&:text)
+end
+
+to_field('quote_title') do |entry, acc|
+  acc.replace entry.all_stencils.flat_map(&:title)
+end
+to_field('quote_manuscript') do |entry, acc|
+  acc.replace entry.all_stencils.flat_map(&:ms).compact.uniq
+end
+
+to_field('quote_md') do |entry, acc|
+  acc.replace entry.all_citations.flat_map(&:md).uniq.compact
+end
+to_field('quote_cd') do |entry, acc|
+  acc.replace entry.all_citations.flat_map(&:cd).uniq.compact
+end
+
+# RIDs
+
+
+each_record do |entry, context|
+  context.clipboard[:rids] = entry.all_stencils.flat_map(&:rid).compact.uniq
+end
+
+to_field('quote_rid') do |entry, acc, context|
+  acc.replace entry.all_stencils.flat_map(&:rid).compact.uniq
+end
+
 
 # Usages
 #      * create tmaps
@@ -72,35 +122,7 @@ to_field('grammatical_usage') do |entry, acc|
   acc.replace entry.senses.flat_map(&:grammatical_usages).compact.uniq
 end
 
-# Etymology and pos
-to_field 'etyma_language', entry_method(:etym_languages)
-to_field 'pos_raw', entry_method(:pos_raw)
-to_field 'pos_abbrev', entry_method(:normalized_pos_raw)
 
-# Quotes
-to_field('quote_text') do |entry, acc|
-  acc.replace entry.all_quotes.map(&:text)
-end
-to_field('quote_md') do |entry, acc|
-  acc.replace entry.all_citations.flat_map(&:md).uniq.compact
-end
-to_field('quote_cd') do |entry, acc|
-  acc.replace entry.all_citations.flat_map(&:cd).uniq.compact
-end
-to_field('quote_title') do |entry, acc|
-  acc.replace entry.all_stencils.flat_map(&:title)
-end
-to_field('quote_manuscript') do |entry, acc|
-  acc.replace entry.all_stencils.flat_map(&:ms)
-end
-
-each_record do |entry, context|
-  context.clipboard[:rids] = entry.all_stencils.flat_map(&:rid)
-end
-
-to_field('quote_rid') do |entry, acc, context|
-  acc.replace context.clipboard[:rids]
-end
 
 
 
