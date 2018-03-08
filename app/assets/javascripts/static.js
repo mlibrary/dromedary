91ed0bbcb0700503c6ce0c23d2a0767a6d175d91
Gordon Leacock <gordonl@umich.edu>
2018-03-08 17:32:12 -0500

    add about/help structure and content
    add fishrappr-like help and about

---
+++ app/assets/javascripts/static.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+  
+  // if we are on a help page (include the contacts page) mark the link for that page in the sidebar
+  $('ul.help-ul li.current a').addClass('currentlyActive');
+
+});
