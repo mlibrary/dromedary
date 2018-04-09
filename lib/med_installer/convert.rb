d44545a377cc9bc54af804dc6ad2d35925ed9fff
Bill Dueber <bill@dueber.com>
2018-04-09 10:57:30 -0400

    Change system to use one json file instead of a zillion
    The creation and lookup of many json files was a real
    burden on the system and made for a very painful
    deployment. This changes it to a single ndj file
    to hold all the serialized entries.

--- lib/med_installer/convert.rb
+++ lib/med_installer/convert.rb
@@ -1,6 +1,6 @@
 require 'middle_english_dictionary'
 require 'hanami/cli'
-require 'concurrent'
+require 'tempfile'
 
 module MedInstaller
   # Convert a bunch of Dromedary xml files into a more useful format.
@@ -10,88 +10,97 @@ module MedInstaller
     desc "Convert the xml files into (faster) json objects (takes a long time)"
 
     argument :datadir, required: true, desc: "The data directory. Converted files will be put in <datadir>/json"
-    argument :dirname, required: false, desc: "one letter to convert"
+    argument :letter, required: false, desc: "one letter to convert"
 
-
-    def new_pool
-      threads = Concurrent.processor_count > 1 ? Concurrent.processor_count - 1 : 1
-      Concurrent::ThreadPoolExecutor.new(
-        min_threads:     threads,
-        max_threads:     threads,
-        max_queue:       100,
-        fallback_policy: :caller_runs
-      )
-    end
 
     def find_oed_file(datapath)
       xmldir = datapath + 'xml'
       xmldir.children.select {|x| x.to_s =~ /MED2OED/}.first
     end
 
-    def call(datadir:, dirname: '*')
+
+    def call(datadir:, letter: nil)
       datapath = Pathname(datadir).realdirpath
       validate_xml_dir(datapath)
 
-      pool = new_pool
-      logger.info "Loading OED links so we can push them into entries"
-      oed    = MiddleEnglishDictionary::Collection::OEDLinkSet.from_xml_file(find_oed_file(datapath))
-      letter = ''
-      Dir.glob("#{datapath}/xml/#{dirname}/MED*xml").each_with_index do |filename, i|
-        logger.info "#{i} done" if i > 0 and i % 2500 == 0
+      tmpfile_name = Pathname(Dir.tmpdir) + 'entries.json.tmp'
+      targetfile = if letter.nil?
+                     datapath + 'entries.json'
+                   else
+                     datapath + "entries_#{letter}.json"
+                   end
+      outfile = File.open(tmpfile_name, 'w:utf-8')
+
+      logger.info "Targeting #{targetfile}"
+      oedfile = find_oed_file(datapath)
+      logger.info "Loading OED links from #{oedfile} so we can push them into entries"
+      oed    = MiddleEnglishDictionary::Collection::OEDLinkSet.from_xml_file(oedfile)
+      count = 0
+      Dir.glob("#{datapath}/xml/#{letter}*/MED*xml").each do |filename|
+        count += 1
+        logger.info "#{count} done" if count > 0 and count % 2500 == 0
         basename, this_letter = letter_and_filename(filename)
         if this_letter != letter
           start_new_letter(datapath, this_letter)
           letter = this_letter
         end
 
-        # pool.post do
-        dump_json(datapath, filename, basename, oed)
-        # end
+        entry = create_and_fill_entry(xmlfilepath: filename, oedlinks: oed)
+        outfile.puts entry.to_json
       end
+      logger.info "Finished converting #{count} entries."
 
-      pool.shutdown
-      pool.wait_for_termination
+      outfile.close
+
+      logger.info "Copying temporary file to real location in #{datapath}"
+
+      # Copy the tempfile over if we made it this far
+      FileUtils.cp(tmpfile_name, targetfile)
     end
 
 
     private
 
-    def dump_json(datapath, filename, basename, oed)
-      entry          = MiddleEnglishDictionary::Entry.new_from_xml_file(filename)
-      entry.oedlink  = oed[entry.id]
-      json_file_name = datapath + 'json' + "#{basename}.json"
-      File.open(json_file_name, 'w:utf-8') {|out| out.puts entry.to_json}
-    rescue MiddleEnglishDictionary::FileNotFound,
-           MiddleEnglishDictionary::FileEmpty,
-           MiddleEnglishDictionary::InvalidXML => e
-      logger.error e.message
-    rescue => e
-      puts e
-      require 'pry'; binding.pry
 
-      puts "Error in #{entry.source}"
-    end
+      # @param [Pathname] xmlfilepath Path to the xml file being processed
+      # @param [MiddleEnglishDictionary::Collection::OEDLinkSet] oedlinks
+      # @return MiddleEnglishDictionary::Entry
+      def create_and_fill_entry(xmlfilepath:, oedlinks:)
+        entry         = MiddleEnglishDictionary::Entry.new_from_xml_file(xmlfilepath)
+        entry.oedlink = oedlinks[entry.id]
+        entry
+      rescue MiddleEnglishDictionary::FileNotFound,
+          MiddleEnglishDictionary::FileEmpty,
+          MiddleEnglishDictionary::InvalidXML => e
+        logger.error e.message
+      rescue => e
+        puts e
+        require 'pry'; binding.pry
 
-
-    def start_new_letter(datapath, this_letter)
-      logger.info "Beginning work on words starting with #{this_letter}"
-      jdir = (datapath + 'json' + this_letter).to_s
-      FileUtils.mkpath(jdir) unless File.exists? jdir
-    end
-
-    def letter_and_filename(f)
-      m           = %r(xml/((.*?)/(.*))\.xml\Z).match(f)
-      this_letter = m[2]
-      this_file   = m[1]
-      return this_file, this_letter
-    end
-
-    def validate_xml_dir(datapath)
-      if Dir.exist?(datapath) and Dir.exist?(datapath + 'xml')
-        logger.info "Found xml directory at #{datapath + 'xml'}"
-      else
-        raise "Can't find xml directory at #{datapath + 'xml'}. Exiting"
+        puts "Error in #{entry.source}"
       end
-    end
+
+      def start_new_letter(datapath, this_letter)
+        logger.info "Beginning work on words starting with #{this_letter}"
+        jdir = (datapath + 'json' + this_letter).to_s
+        FileUtils.mkpath(jdir) unless File.exists? jdir
+      end
+
+
+      def letter_and_filename(f)
+        m           = %r(xml/((.*?)/(.*))\.xml\Z).match(f)
+        this_letter = m[2]
+        this_file   = m[1]
+        return this_file, this_letter
+      end
+
+
+      def validate_xml_dir(datapath)
+        if Dir.exist?(datapath) and Dir.exist?(datapath + 'xml')
+          logger.info "Found xml directory at #{datapath + 'xml'}"
+        else
+          raise "Can't find xml directory at #{datapath + 'xml'}. Exiting"
+        end
+      end
   end
 end
