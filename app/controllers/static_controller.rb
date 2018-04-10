4a2d8e3a3fc41e96f2572daab84c743a447c17fa
Gordon Leacock <gordonl@umich.edu>
2018-04-10 12:59:50 -0400

    add two level menus; add app part to site-title

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,13 @@
 class StaticController < ApplicationController
-  layout 'static'
-end
+
+  def help_med
+  	@current_action = 'help'
+  	render :layout => 'static'
+  end
+
+  def about_med
+  	@current_action = 'about'
+  	render :layout => 'static'
+  end
+
+ end
