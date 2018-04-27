f8323176d394b2766d260aea047720af5a801a90
Bill Dueber <bill@dueber.com>
2018-04-27 17:39:17 -0400

    Use XSL Transforms to display data
    Change display of data in the Show page for a dictionary
    entry to use XSL transforms found in `indexer/xslt`.
    
      * Changes to the presenter to generate XML nodes that
        can be transformed and then provide the raw HTML
        to the views
      * Split the Show view into a couple files to make it
        easier to read
      * Change the expand/collapse javascript to target the
        entire block that needs hiding.

--- indexer/writers/localhost.rb
+++ indexer/writers/localhost.rb
@@ -6,7 +6,7 @@ require 'annoying_utilities'
 
 settings do
   provide "solr.url", AnnoyingUtilities.solr_url
-  provide "solr_writer.commit_on_close", "true"
+  provide "solr_writer.commit_on_close", "false"
   provide "solr_writer.thread_pool", 2
   provide "solr_writer.batch_size", 200
   provide "writer_class_name", "Traject::SolrJsonWriter"
