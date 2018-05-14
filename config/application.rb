e2fbab321c4217df62df2ab9f1643577b528923c
Aaron Elkiss <aelkiss@umich.edu>
2018-05-14 11:57:11 -0400

    use relative_url_root

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module MiddleEnglishDictionary
 
     config.autoload_paths << "#{Rails.root}/lib"
 
+    config.relative_url_root = MiddleEnglishDictionary.config.relative_url_root
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
