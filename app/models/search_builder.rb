78fb61430d9c0c3de30c6c177a125377af9cdd8e
Bill Dueber <bill@dueber.com>
2018-06-08 09:29:25 -0400

    First shot at fixing intersticial-parens stuff

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -2,6 +2,27 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
 
+  self.default_processor_chain += [:escape_intersticial_parens]
+
+
+  # q"=>"{!qf=$quote_everything_qf pf=$quote_everything_pf}right",
+  #
+  # We'll say that no matter what is in 'q', we'll escape parens
+  # for anything that's not in braces
+  #
+  # This might be a problem with the advanced search...
+
+  EscapeWorthy = /([-\p{Alpha}])\((\p{Alpha}{1,3})\)/
+
+
+  def escape_intersticial_parens(solr_params)
+    current_q = solr_params['q']
+    if current_q
+      new_q            = current_q.gsub EscapeWorthy, '\1\\\\(\2\\\\)'
+      solr_params['q'] = new_q
+    end
+  end
+
   ##
   # @example Adding a new step to the processor chain
   #   self.default_processor_chain += [:add_custom_data_to_query]
