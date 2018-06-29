5c9b27df28df5321666f5057c56130a195aac665
Bridget Burke <burkebri@umich.edu>
2018-06-29 13:16:39 -0400

    MEC-16 Add special characters in search string

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -3,4 +3,24 @@ $( document ).ready(function() {
   // if we are on a help page (include the contacts page) mark the link for that page in the sidebar
   $('ul.help-ul li.current a').addClass('currentlyActive');
 
+  // Special character keyboard entry for search form input
+    $('#thorn').on('click', function () {
+        var text = $('#q');
+        text.val(text.val() + 'þ');    
+    });
+    
+    $('#eth').on('click', function () {
+        var text = $('#q');
+        text.val(text.val() + 'ð');    
+    });
+    
+     $('#yogh').on('click', function () {
+        var text = $('#q');
+        text.val(text.val() + 'ȝ');    
+    });
+     $('#ash').on('click', function () {
+        var text = $('#q');
+        text.val(text.val() + 'æ');    
+    });
+
 });
