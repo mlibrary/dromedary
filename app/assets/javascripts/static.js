85f4a230eee00f1e5ae7938685cf4110eef29c63
Bridget Burke <burkebri@umich.edu>
2018-07-11 20:58:20 -0400

    MEC-188 Beginning styling for About

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -3,6 +3,12 @@ $( document ).ready(function() {
   // if we are on a help page (include the contacts page) mark the link for that page in the sidebar
   $('ul.help-ul li.current a').addClass('currentlyActive');
 
+  // for about page sidebar links, mark li as current/active on click
+  $("ul.help-ul li").click(function() {
+      $("ul.help-ul li").removeClass("current");
+      $(this).addClass("current");
+   });
+
   // Special character keyboard entry for search form input
     $('#thorn').on('click', function () {
         var text = $('#q');
