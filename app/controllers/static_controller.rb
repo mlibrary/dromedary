adcb9d7df240b3ad7df0c41d1291caa67231ce7a
Bill Dueber <bill@dueber.com>
2018-07-19 00:59:41 -0400

    More route and text cleanup

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,18 +1,9 @@
 class StaticController < ApplicationController
 
-  def help_med
-  	@current_action = 'help'
-  	render :layout => 'static'
-  end
-
-  def help
-    @current_action = "help"
-    render "static/help/dictionary", layout: 'static'
-  end
 
   def about_med
   	@current_action = 'about'
-  	render :layout => 'static'
+  	render 'static/about_med', :layout => 'static'
   end
 
  end
