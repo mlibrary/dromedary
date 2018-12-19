747d44f6bf5cda5b72a9cf03b47b4a029c926ed9
Bill Dueber <bill@dueber.com>
2018-12-19 11:42:15 -0500

    Index prefix/suffix with leading- or trailing- dash
    Searches for, e.g., `-ward` were incorrectly handled in
    two ways:
      * The `-` was interpreted as a unary NOT (part of edismax)
      * The `-` was being thrown away as a junk charactor and/or
        as a word boundary.
    
    This commit does the following:
      * Add a function to the pipline in `search_builder.rb`
        that escapes leading or trailing dashes before they
        get sent up to Solr, to avoid the unary negative
        interpretation.
      * Adds a new fieldType `me_prefix_suffix` that acts like
        an exactish ME field (in terms of latinizing for search)
        but protects a leading- or trailing dash
      * Create a new field, `prefix_suffix` of that type
      * Populate the field with regularized headwords iff
        the headword has a leading or trailing dash
      * Make a search matching the `prefix_suffix` field
        the (currently) most powerful search field.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -2,7 +2,10 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
 
-  self.default_processor_chain += [:yogh_to_ezh, :escape_intersticial_parens, :default_to_everything_search]
+  self.default_processor_chain += [:yogh_to_ezh,
+                                   :escape_intersticial_parens,
+                                   :escape_prefix_suffix_dash,
+                                   :default_to_everything_search]
 
 
   # q"=>"{!qf=$quote_everything_qf pf=$quote_everything_pf}right",
@@ -12,7 +15,7 @@ class SearchBuilder < Blacklight::SearchBuilder
   #
   # This might be a problem with the advanced search...
 
-  EscapeWorthy = /([-\p{Alpha}])\((\p{Alpha}{1,3})\)/
+  Parens_EscapeWorthy = /([-\p{Alpha}])\((\p{Alpha}{1,3})\)/
 
 
   # TODO: Get these values from blacklight_config
@@ -22,17 +25,41 @@ class SearchBuilder < Blacklight::SearchBuilder
      'quotes' => 'quote_date_sort asc, author_sort asc'
   }
 
+
+  # These two characters are equivalent
   def yogh_to_ezh(solr_params)
     solr_params['q'] = solr_params['q'].gsub(/[Ȝȝ]/, 'ʒ')
   end
 
+  # Sometimes, you gotta escape the parens so things parse right
   def escape_intersticial_parens(solr_params)
     current_q = solr_params['q']
     if current_q
-      new_q            = current_q.gsub EscapeWorthy, '\1\\\\(\2\\\\)'
+      new_q            = current_q.gsub Parens_EscapeWorthy, '\1\\\\(\2\\\\)'
       solr_params['q'] = new_q
       solr_params['debug'] = 'true'
     end
+  end
+
+  # There are prefixes and suffixes in the dictionary -- we want to
+  # escape the leading- or trailing- dashes
+  #
+  # This will also have the effect of making '-' no longer work
+  # as a unary NOT operator.
+  #
+  # Note that the solr query is generated as something like
+  # {!qf=$headword_and_forms_qf pf=$headword_and_forms_pf}-ward
+  # ...so the beginning of the prefix actually follows a '}'
+
+  def escape_prefix_suffix_dash(solr_params)
+    q = solr_params['q']
+    oq = q
+    q = q.gsub(/\}\-/, '}\\\\-')
+    q = q.gsub(/\s+-/, ' \\\\-')
+    q = q.gsub(/\-\s+/, '\\\\- ')
+    q = q.gsub(/\-\Z/, '\\\\-')
+    solr_params['q'] = q
+    File.open('dash.txt', 'w:utf-8') {|f| f.puts "#{oq}\n#{q}"}
   end
 
   def default_to_everything_search(solr_params)
