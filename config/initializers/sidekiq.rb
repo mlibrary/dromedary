1546643e5935ef733656b8756d6c4d306fcd3538
Anthony Thomas <esty@umich.edu>
2022-06-23 15:21:38 -0400

    fix sidekiq, somehow

--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], size: 4, network_timeout: 5 }
-end
