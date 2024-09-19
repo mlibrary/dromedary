2dfd25ae7bd082f3550184ace987bbd78f9eff8c
Bill Dueber <bill@dueber.com>
2024-09-18 22:00:31 -0400

    Clean dead files out of `lib/med_installer`
    Based on using `Simplecov` in the `bin/index_new_file.rb`
    script. There's still plenty of dead code, but this will help.
    
    The root file for indexing is `lib/med_installer/indexing_steps.rb`

--- lib/med_installer.rb
+++ lib/med_installer.rb
@@ -3,17 +3,7 @@ require "middle_english_dictionary"
 
 require_relative "med_installer/logger"
 require_relative "med_installer/extract"
-require_relative "med_installer/index"
 require_relative "med_installer/convert"
-require_relative "med_installer/solr"
 require_relative "med_installer/indexer/entry_json_reader"
 require_relative "med_installer/indexer/bib_reader"
-require_relative "med_installer/extract_convert_index"
-
-require_relative "med_installer/remote"
-require_relative "med_installer/control"
-
-require_relative "med_installer/copy_from_build"
-require_relative "med_installer/index_new_data"
-
 require_relative "med_installer/ping_prometheus"
