f3731d0d35c5dbf6e1061f64e6ceb67a17694260
Bill Dueber <bill@dueber.com>
2024-09-24 14:57:34 -0400

    Use a memoizing ttl library to reduce solr calls.
    The implementation of `solr_cloud-connection` is purposefully chatty,
    not caching anything because during the admin cycle, you want to see
    your changes reflected immediatley and the number of operations is generally small.
    
    My use of uncached values (checking the name of the collection underlying an alias) on what
    turned out to be basically every call was disastrous.
    
    This PR creates use `ttl_memoizeable` to memoize the two calls for data that can
    change after a new upload: the hyp_to_bib data, and the date of indexing (for the
    footer).
    
    In theory we should just have some sort of a listener that tells us when
    the underlying collection has changed and have that trigger things,
    but we don't, so here we are.
    
     Notable changes are:
       * Two methods in load_local_config now memoize values for the
         hyperbib mapping and the name of the current underlying
         connection.
       * The formerly-recursive method of determining the underlying collection
         name (because it's legal, in general, to have aliass that point to other
         aliases) has been removed, since we just don't need it.
       * The footer now shows, ridiculously, the time down to the second.
         This is purely to make testing easier, because seeing a change in
         the footer date is an easy way to know the change has been made.

--- config/load_local_config.rb
+++ config/load_local_config.rb
@@ -3,9 +3,11 @@ require "pathname"
 require_relative "../lib/med_installer/logger"
 require_relative "../lib/dromedary/services"
 require_relative "../lib/med_installer/hyp_to_bibid"
+require "ttl_memoizeable"
 
 module Dromedary
   class << self
+    extend TTLMemoizeable
     def logger
       Rails.logger || MedInstaller::Logger::LOGGER
     end
@@ -31,15 +33,24 @@ module Dromedary
     end
 
 
-
     def hyp_to_bibid
-      Dromedary::Services[:hyp_to_bibid].value
+      collection = Dromedary::Services[:solr_current_collection]
+      Rails.logger.warn "################# Fetching HyperBib ########################"
+      MedInstaller::HypToBibId.get_from_solr(collection: collection)
     end
-
 
     def collection_creation_date
-      Dromedary::Services[:collection_creation_date].value
+      Rails.logger.warn "################# Fetching creation date ########################"
+      collection = Dromedary::Services[:solr_current_collection]
+      if collection
+        Dromedary.compute_collection_creation_date collection.collection.name
+      else
+        "Never"
+      end
     end
+
+    ttl_memoized_method :hyp_to_bibid, ttl: 20.seconds
+    ttl_memoized_method :collection_creation_date, ttl: 20.seconds
 
     def collection_creation_date_string
       collection_creation_date.strftime("%A, %B %-e, %Y at %H:%M:%S")
